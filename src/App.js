077598debcf35192a2e0b131d06138568a2a59f1
Allan Thomas <allanr@Torus.Tech>
2024-10-21 15:57:14 +0530

    Check push

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="w-full h-full">
       <div className="text-3xl w-full h-full text-blue-400">Demo</div>
+      <div className="text-red-500 font-mono font-bold"> Git</div>
     </div>
   );
 }
