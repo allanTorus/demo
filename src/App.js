d739933deac88ddcc5fca302a8411661c6275726
Allan Thomas <132202838+allanthomas22@users.noreply.github.com>
2024-10-21 15:53:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="w-full h-full">
+      <div className="text-3xl w-full h-full text-blue-400">Demo</div>
+    </div>
+  );
+}
